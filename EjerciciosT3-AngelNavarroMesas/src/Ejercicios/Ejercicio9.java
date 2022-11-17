e9be3acc122fa363942a903371a492ee299cca24
anavarro <anavarro@106-30.info2>
2022-11-17 14:37:45 +0100

    comit del 10 sin terminar

--- EjerciciosT3-AngelNavarroMesas/src/Ejercicios/Ejercicio9.java
+++ EjerciciosT3-AngelNavarroMesas/src/Ejercicios/Ejercicio9.java
@@ -7,11 +7,14 @@ public class Ejercicio9 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
+		//Declaramos las variables
 		int num, dig=0;
 		
+		//Pedimos al usuario que introduzca un numero y lo escanemaos
 		System.out.println("Introduzca un numero para averiguar sus cifras");
 		num = sc.nextInt();
 		
+		//Si el numero es 0 o menor que 0 se le indica al usuario que el numero introducido debe ser mayor que 0
 		if(num<=0) {
 			System.out.println("El numero introducido debe ser mayor que 0");
 		}else {
